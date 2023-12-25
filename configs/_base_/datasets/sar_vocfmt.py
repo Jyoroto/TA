bab0f65fd70fceac1e27da62cf5446b42dd1178a
HuaDeity <hello@huadeity.com>
2023-12-25 14:01:38 +0800

    Init Sar Config

---
+++ configs/_base_/datasets/sar_vocfmt.py
@@ -0,0 +1,73 @@
+# dataset settings
+dataset_type = 'VOCDataset'
+data_root = "/data6/zhaochenxu/web103/"
+
+# Method 2: Use `backend_args`, `file_client_args` in versions before 3.0.0rc6
+# backend_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/segmentation/',
+#         'data/': 's3://openmmlab/datasets/segmentation/'
+#     }))
+backend_args = None
+
+metainfo = {
+    'classes': ('A220', 'A320/321', 'A330', 'ARJ21', 'Boeing737', 'Boeing787', 'other'),
+}
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(1280, 1280), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(1280, 1280), keep_ratio=True),
+    # avoid bboxes being resized
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='VOC2012-sim/ImageSets/Main/trainval.txt',
+        data_prefix=dict(sub_data_root='VOC2012-sim/'),
+        filter_cfg=dict(
+        filter_empty_gt=True, min_size=32, bbox_min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args)
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='VOC2012-sim/ImageSets/Main/test.txt',
+        data_prefix=dict(sub_data_root='VOC2012-sim/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+test_dataloader = val_dataloader
+
+# Pascal VOC2007 uses `11points` as default evaluate mode, while PASCAL
+# VOC2012 defaults to use 'area'.
+val_evaluator = dict(type='VOCMetric', metric='mAP', eval_mode='area')
+test_evaluator = val_evaluator
